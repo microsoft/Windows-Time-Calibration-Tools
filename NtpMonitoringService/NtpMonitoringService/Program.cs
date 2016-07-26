68c925dc0bf4c581bee97aa2a9dfdebf4b26707f
Alan Jowett <alanjo@microsoft.com>
2016-07-26 12:39:49 -0700

    Service to monitor a set of NTP server

---
+++ NtpMonitoringService/NtpMonitoringService/Program.cs
@@ -0,0 +1,20 @@
+﻿using System.ServiceProcess;
+
+namespace NtpMonitoringService
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        static void Main()
+        {
+            ServiceBase[] ServicesToRun;
+            ServicesToRun = new ServiceBase[]
+            {
+                new Monitoring()
+            };
+            ServiceBase.Run(ServicesToRun);
+        }
+    }
+}
