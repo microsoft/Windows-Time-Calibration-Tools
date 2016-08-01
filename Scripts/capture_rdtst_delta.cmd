3ed0193c711042b7e5b4f8624b437f543917407f
Alan Jowett <alanjo@microsoft.com>
2016-08-01 11:23:44 -0700

    Add script to dump TSC delta between partitions (#2)

---
+++ Scripts/capture_rdtst_delta.cmd
@@ -0,0 +1,5 @@
+tracelog -start rdtsc -f hyper-v.etl -b 1024 -min 8 -max 8 -batched
+tracelog -enableex rdtsc -guid #910C653D-A4EB-4719-B909-4588E3BAEC91 -matchanykw 0x0000000000002000
+pause
+tracelog -stop rdtsc
+netsh trace convert hyper-v.etl hyper-v.xml xml
