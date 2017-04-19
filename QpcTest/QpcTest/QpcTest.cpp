4c41720bb2955ed4dcbbbf3fcc702e2ff807bbf1
Alan Jowett <alanjo@microsoft.com>
2017-04-19 16:55:25 -0700

    Add QPC and __rdtsc

--- QpcTest/QpcTest/QpcTest.cpp
+++ QpcTest/QpcTest/QpcTest.cpp
@@ -13,7 +13,7 @@ int main()
 		LARGE_INTEGER ts1, ts2, freq;
 		__int64 total = 0;
 		QueryPerformanceCounter(&ts1);
-		for (long long i = 0; i < 1000000000; i++)
+		for (long long i = 0; i < 1000000; i++)
 		{
 			GetSystemTimePreciseAsFileTime(&ft);
 		}
@@ -23,7 +23,46 @@ int main()
 		QueryPerformanceFrequency(&freq);
 		double queryTime = total;
 		queryTime /= freq.QuadPart;
-		printf("%.3f\n", queryTime);
+		printf("GetSystemTimePreciseAsFileTime latency %.3f\n", queryTime*1000);
+	}
+	for (int j = 0; j < 10; j++)
+	{
+		LARGE_INTEGER ft;
+
+		LARGE_INTEGER ts1, ts2, freq;
+		__int64 total = 0;
+		QueryPerformanceCounter(&ts1);
+		for (long long i = 0; i < 1000000; i++)
+		{
+			QueryPerformanceCounter(&ft);
+		}
+		QueryPerformanceCounter(&ts2);
+		total += ts2.QuadPart - ts1.QuadPart;
+
+		QueryPerformanceFrequency(&freq);
+		double queryTime = total;
+		queryTime /= freq.QuadPart;
+		printf("QueryPerformanceCounter latency %.3f\n", queryTime*1000);
+	}
+
+	for (int j = 0; j < 10; j++)
+	{
+		DWORD64 ft;
+
+		LARGE_INTEGER ts1, ts2, freq;
+		__int64 total = 0;
+		QueryPerformanceCounter(&ts1);
+		for (long long i = 0; i < 1000000; i++)
+		{
+			ft = __rdtsc();
+		}
+		QueryPerformanceCounter(&ts2);
+		total += ts2.QuadPart - ts1.QuadPart;
+
+		QueryPerformanceFrequency(&freq);
+		double queryTime = total;
+		queryTime /= freq.QuadPart;
+		printf("__rdtsc latency %.3f\n", queryTime*1000);
 	}
 
 	return 0;
