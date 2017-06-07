20ec5308bb62c2eacef0253be4c8d10d1dab88c7
Alan Jowett <alanjo@microsoft.com>
2017-06-07 11:36:20 -0700

    Fix re-entrant timer issue

--- NtpMonitoringService/NtpMonitoringService/Monitoring.cs
+++ NtpMonitoringService/NtpMonitoringService/Monitoring.cs
@@ -178,7 +178,7 @@ namespace NtpMonitoringService
             Shutdown = new System.Threading.ManualResetEvent(false);
 
             ConfigRefresh = new System.Threading.Timer((object o) => { UpdateServerList(); });
-            ConfigRefresh.Change(TimeSpan.FromSeconds(60), TimeSpan.FromMilliseconds(-1));
+            ConfigRefresh.Change(TimeSpan.FromSeconds(0), TimeSpan.FromMilliseconds(-1));
         }
 
         protected override void OnStop()
