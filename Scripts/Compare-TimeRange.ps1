99a49451935d66c5622e481bea41570d6ee882de
Paul Long <paullo@microsoft.com>
2017-07-06 15:14:30 -0700

    Colomn Fix

--- Scripts/Compare-TimeRange.ps1
+++ Scripts/Compare-TimeRange.ps1
@@ -3,7 +3,10 @@
    [string]$f1,
 
    [Parameter(Mandatory=$True, Position=2)]
-   [string]$f2
+   [string]$f2,
+
+   [Parameter(Mandatory=$True, Position=3)]
+   [int]$startcol
 )
 
 $t1h = ConvertFrom-csv (type $f1 -head 5 | select -last 1) -header a,b,c,d
@@ -11,10 +14,20 @@ $t1t = ConvertFrom-csv (type $f1 -tail 1) -header a,b,c,d
 $t2t = ConvertFrom-csv (type $f2 -tail 1) -header a,b,c,d
 $t2h = ConvertFrom-csv (type $f2 -head 5 | select -last 1 ) -header a,b,c,d
 
-$sut_beg = [DateTime]::FromFileTime($t1h.c)
-$sut_end = [DateTime]::FromFileTime($t2h.c)
-$ref_beg = [DateTime]::FromFileTime($t2t.c)
-$ref_end = [DateTime]::FromFileTime($t1t.c)
+if($startcol -eq 0)
+{
+    $sut_beg = [DateTime]::FromFileTime($t1h.c)
+    $sut_end = [DateTime]::FromFileTime($t2h.c)
+    $ref_beg = [DateTime]::FromFileTime($t2t.c)
+    $ref_end = [DateTime]::FromFileTime($t1t.c)
+}
+else
+{
+    $sut_beg = [DateTime]::FromFileTime($t1h.d)
+    $sut_end = [DateTime]::FromFileTime($t2h.d)
+    $ref_beg = [DateTime]::FromFileTime($t2t.d)
+    $ref_end = [DateTime]::FromFileTime($t1t.d)
+}
 
 echo ("Ref beg: " + $ref_beg.ToString("u"))
 echo ("  Sut beg: " + $sut_beg.ToString("u"))
