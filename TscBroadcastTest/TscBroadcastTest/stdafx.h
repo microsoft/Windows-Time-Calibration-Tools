ce09397b6ff4e404fa87d912c946a2d85a145a7a
Alan Jowett <alanjo@microsoft.com>
2018-01-19 10:31:10 -0800

    Add tool to measure TSC offset between cores (#52)
    * PingPing
    
    * simplified
    
    * Simplify
    
    * Simplify
    
    * Working
    
    * Clean version with stats
    
    * Better stats
    
    * Add makefile for linux
    
    * Fix encoding
    
    * fix encoding
    
    * Fix encoding
    
    * Fix MSVC specifics
    
    * Fix MSVC specifics
    
    * Fix MSVC specifics
    
    * Fix MSVC specifics
    
    * Fix MSVC specifics
    
    * Fix MSVC specifics
    
    * Cleanup

---
+++ TscBroadcastTest/TscBroadcastTest/stdafx.h
@@ -0,0 +1,18 @@
+// stdafx.h : include file for standard system include files,
+// or project specific include files that are used frequently, but
+// are changed infrequently
+//
+
+#pragma once
+
+#if defined(_MSC_VER)
+#include "targetver.h"
+#endif
+
+#include <stdio.h>
+#include "platform.h"
+
+
+
+// TODO: reference additional headers your program requires here
+
